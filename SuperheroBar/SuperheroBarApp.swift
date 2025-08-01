8ec60d7d73c5b1bd43f127050babe91a9fabd885
Abby Dominguez <abbytorade@icloud.com>
2025-08-01 15:03:39 +0200

    Initial Commit

---
+++ SuperheroBar/SuperheroBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperheroBarApp.swift
+//  SuperheroBar
+//
+//  Created by Abby Dominguez on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SuperheroBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
