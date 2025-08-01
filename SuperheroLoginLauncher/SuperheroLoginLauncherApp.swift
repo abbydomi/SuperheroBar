22462c3a0dde15ac9719f60a0d4eadf7c28da587
Abby Dominguez <abbytorade@icloud.com>
2025-08-01 18:11:04 +0200

    🐛 Fixed Login item crash

--- SuperheroLoginLauncher/SuperheroLoginLauncherApp.swift
+++ SuperheroLoginLauncher/SuperheroLoginLauncherApp.swift
@@ -9,27 +9,33 @@ import SwiftUI
 
 @main
 struct SuperheroLoginLauncherApp: App {
+
+    @Environment(\.scenePhase) private var scenePhase
+
     var body: some Scene {
         WindowGroup {
             EmptyView()
+                .onAppear {
+                    launchMainApp()
+                }
         }
         .commands {
             CommandGroup(replacing: .newItem) { }
         }
     }
 
-    init() {
-        launchMainApp()
-        NSApp.terminate(nil)
-    }
-
     func launchMainApp() {
         let mainAppBundleId = "com.abbydomi.SuperheroBar"
         if !NSWorkspace.shared.launchApplication(withBundleIdentifier: mainAppBundleId,
-                                                         options: [.default],
-                                                         additionalEventParamDescriptor: nil,
-                                              launchIdentifier: nil) {
+                                                 options: [.default],
+                                                 additionalEventParamDescriptor: nil,
+                                                 launchIdentifier: nil) {
             print("Failed to launch the main app!")
+        } else {
+            // Terminate after launching the main app, with a slight delay to avoid killing too early
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                NSApp.terminate(nil)
+            }
         }
     }
 }
